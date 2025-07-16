d73b931f994d04b830c380da7cc5d4c193f3c944
FieldHouser <hfeldthus@gmail.com>
2025-07-16 17:44:23 +0200

    Super rough implementation of eta1 CI

--- waveletvar.py
+++ waveletvar.py
@@ -1,6 +1,6 @@
 import pywt
 import numpy as np
-from scipy.stats import norm
+from scipy.stats import norm, chi2
 from modwt import modwt
 
 
@@ -13,6 +13,33 @@ def wavelet_variance(x, filters, level):
     interior_coeffs = w[level][scale_length-1:]
     var = np.sum(interior_coeffs ** 2)/n_interior_coeffs
     return var
+
+def wavelet_variance_CI(x, filters , level, alpha):
+    var_estim = wavelet_variance(x, filters, level)
+    wavelet = pywt.Wavelet(filters)
+    filter_length = wavelet.dec_len
+    w = modwt(x, filters, level)
+    N = len(x)
+    scale_length = (2**level-1)*(filter_length-1) + 1
+    n_interior_coeffs = N - scale_length + 1
+    total = 0.0
+    for tau in range(1, n_interior_coeffs):
+        indices = np.arange(scale_length - 1, N - tau)
+        products = w[level-1][indices]*w[level-1][indices+tau]
+        s_tau = np.sum(products)/(n_interior_coeffs)
+        total += s_tau**2
+
+    A = (var_estim**2)/2 + total
+    
+
+    
+    df = n_interior_coeffs*(var_estim**2)/A
+    
+    U = var_estim*df / chi2.ppf(alpha/2, df)
+    L =  var_estim*df / chi2.ppf(1-alpha/2, df)
+    return [L, var_estim, U]
+
+
 
 
 def wavelet_cross_covariance(x, y, filters, level, lag):
